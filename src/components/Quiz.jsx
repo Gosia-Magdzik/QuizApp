54efbbefd9e254f9f9b82e882160eb451103947d
Gosia Magdzik <melassa26@gmail.com>
2024-01-26 16:57:33 +0100

    handleSelectAnswer f. added

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,23 +2,35 @@ import React, { useState } from 'react';
 import questions from '../questions';
 
 export const Quiz = () => {
-
     const [userAnswers, setUserAnswer] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+    const fixedAnswer = [...questions[activeQuestionIndex].answers];
+    fixedAnswer.sort(() => Math.random() - 0.5);
+
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswer((prevUserAnswer) => {
+            return [...prevUserAnswer, selectedAnswer];
+        });
+    }
 
     return (
-        <div id="question" >
+        <div id="quiz">
+            <div id="question" >
             <h2>{questions[activeQuestionIndex].text}</h2>
             <ul id="answers">
                 {questions[activeQuestionIndex].answers.map(answer => (
                    <li key={answer} className='answer'>
-                        <button>
+                        <button onClick={() => handleSelectAnswer(answer)}>
                             {answer}
                         </button>
                    </li> 
                 ))}
             </ul>
         </div>
+        </div>
     )
 }
+
+
+
