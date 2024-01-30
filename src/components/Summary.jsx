012c77bf1a36ce097c22f12af84a2693028f2f12
Gosia Magdzik <melassa26@gmail.com>
2024-01-30 15:33:03 +0100

    added condition for css classes depending on answer

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -25,15 +25,24 @@ export const Summary = ({ userAnswers }) => {
             </div>
             <ol>
                 {userAnswers.map((answer, index) => {
+                    let cssClass = 'user-answer';
+
+                    if (answer === null) {
+                        cssClass += ' skipped';
+                    } else if (answer === questions[index].answers[0]) {
+                        cssClass += ' correct';
+                    } else {
+                        cssClass += ' wrong';
+                    }
+
                     return (
                     <li key={answer}>
                         <h3>{index + 1}</h3>
                         <p className='question'>{questions[index].text}</p>
-                        <p className='user-answer'>{answer}</p>
+                        <p className={cssClass}>{answer ?? 'Skipped'}</p>
                     </li>
                 )
                 })}
-
             </ol>
         </div>
   )
