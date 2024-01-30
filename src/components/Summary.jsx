422d9d3a7280a6252bcfd590558e420403336c4d
Gosia Magdzik <melassa26@gmail.com>
2024-01-30 15:21:16 +0100

    media added

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import quizCompleted from '../assets/quiz-complete.png'
+import questions from '../questions';
 
 export const Summary = ({ userAnswers }) => {
-  return (
+
+
+    return (
         <div id='summary'>
             <img src={quizCompleted} />
             <h2>Quiz Completed</h2>
@@ -21,11 +24,16 @@ export const Summary = ({ userAnswers }) => {
                 </p>
             </div>
             <ol>
-                <li>
-                    <h3>2</h3>
-                    <p className='question'>question text</p>
-                    <p className='user-answer'>user's answer</p>
-                </li>
+                {userAnswers.map((answer, index) => {
+                    return (
+                    <li key={answer}>
+                        <h3>{index + 1}</h3>
+                        <p className='question'>{questions[index].text}</p>
+                        <p className='user-answer'>{answer}</p>
+                    </li>
+                )
+                })}
+
             </ol>
         </div>
   )
